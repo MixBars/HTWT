aa15a1508b66e284a3f7f3f39ee0552db4a3436d
MixBars <portalmixbars@gmail.com>
2021-11-11 09:56:06 -0800

    fix locale + add devise (not configured)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
  around_action :switch_locale
  
+ 
  def default_url_options
    { locale: I18n.locale }
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   scope '(:locale)', locale: /en|ru/ do
-        
+    devise_for :users
+
     root to: 'reviews#index'
   end
 end
