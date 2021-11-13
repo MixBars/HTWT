eb700b658ff756314139c3b17ef45a3d152a1654
MixBars <portalmixbars@gmail.com>
2021-11-13 09:40:14 -0800

    some frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do  
   
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   scope '(:locale)', locale: /en|ru/ do
     
-
+    devise_for :users, skip: :omniauth_callbacks
     resources :reviews
 
     root to: 'reviews#index'
